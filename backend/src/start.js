255612d35d2fb533602f0c3185ec483d1cbec146
Rafaelle Pessoa <controlect2026@gmail.com>
2026-08-31 13:22:50 -0400

    Executa seed no start do backend

--- backend/src/start.js
+++ backend/src/start.js
@@ -2,6 +2,7 @@ const { execSync } = require('child_process');
 
 if (process.env.DATABASE_URL) {
   execSync('npx prisma migrate deploy', { stdio: 'inherit' });
+  execSync('npx prisma db seed', { stdio: 'inherit' });
 }
 
 require('./server');
